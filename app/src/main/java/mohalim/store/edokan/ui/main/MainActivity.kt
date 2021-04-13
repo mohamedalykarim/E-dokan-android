5fcbca1c853612aa04c0ef6222dc4d82cf8381d4
mohamed.aly.karim@gmail.com <Mohalim654326>
2021-04-14 01:58:38 +0200

    viewpager2 update

--- app/src/main/java/mohalim/store/edokan/ui/main/MainActivity.kt
+++ app/src/main/java/mohalim/store/edokan/ui/main/MainActivity.kt
@@ -1,35 +1,27 @@
 package mohalim.store.edokan.ui.main
 
-import android.animation.Animator
-import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.AnimationDrawable
-import android.graphics.drawable.TransitionDrawable
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.transition.Slide
-import android.transition.Transition
-import android.transition.TransitionManager
-import android.util.Log
-import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
-import android.view.animation.Animation
-import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.fragment.app.FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import dagger.hilt.android.AndroidEntryPoint
 import mohalim.store.edokan.R
 import mohalim.store.edokan.databinding.ActivityMainBinding
-import mohalim.store.edokan.ui.login.LoginActivity
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -51,7 +43,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.pager.adapter = HomePagerAdapter(
                 supportFragmentManager,
-                BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT,
+                lifecycle,
                 homeFragment,
                 cartFragment,
                 accountFragment
@@ -59,104 +51,31 @@ class MainActivity : AppCompatActivity() {
 
         binding.pager.currentItem = 2
 
+        binding.pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if(position == 2) homeBottom()
+                if (position == 1) cartBottom()
+                if (position == 0) accountBottom()
+            }
+        })
+
         handleBottomClicks();
     }
 
     private fun handleBottomClicks() {
-        val params1 : LinearLayout.LayoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
-        val params2 : LinearLayout.LayoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
 
 
         binding.homeIcon.setOnClickListener(View.OnClickListener {
-            if (viewmodel.currentTab == viewmodel.HOME) return@OnClickListener
-            viewmodel.currentTab = viewmodel.HOME
-            binding.pager.currentItem = 2
-
-            binding.homeContainerBG.setBackgroundResource(R.drawable.bottom_bar_item_bg)
-            binding.cartContainerBG.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
-            binding.accountContainerBG.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
-
-            binding.homeContainerBG.translationY = (binding.bottomContainer.height- binding.homeContainerBG.height).toFloat()
-            binding.homeContainerBG.animate().setDuration(200).translationY(0f).setListener(null)
-
-
-            params1.weight = 2f
-            binding.homeContainer.layoutParams = params1;
-
-            params2.weight = 3f
-            binding.cartContainer.layoutParams = params2;
-            binding.accountContainer.layoutParams = params2;
-
-            binding.homeIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_main_active))
-            binding.cartIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_cart_not_active))
-            binding.accountIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_account_not_active))
-
-            binding.homeIcontTv.visibility = View.VISIBLE
-            binding.cartIconTv.visibility = View.GONE
-            binding.accountIconTv.visibility = View.GONE
-
-
+            homeBottom();
         })
 
         binding.cartIcon.setOnClickListener(View.OnClickListener {
-            if (viewmodel.currentTab == viewmodel.CART) return@OnClickListener
-            viewmodel.currentTab = viewmodel.CART
-            binding.pager.currentItem = 1
-
-            binding.cartContainerBG.setBackgroundResource(R.drawable.bottom_bar_item_bg)
-            binding.homeContainerBG.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
-            binding.accountContainerBG.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
-
-            binding.cartContainerBG.translationY = (binding.bottomContainer.height - binding.cartContainerBG.height).toFloat()
-            binding.cartContainerBG.animate().setDuration(200).translationY(0f).setListener(null)
-
-
-
-            params1.weight = 2f
-            binding.cartContainer.layoutParams = params1;
-
-            params2.weight = 3f
-            binding.homeContainer.layoutParams = params2;
-            binding.accountContainer.layoutParams = params2;
-
-            binding.homeIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_main_not_active))
-            binding.cartIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_cart_active))
-            binding.accountIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_account_not_active))
-
-            binding.homeIcontTv.visibility = View.GONE
-            binding.cartIconTv.visibility = View.VISIBLE
-            binding.accountIconTv.visibility = View.GONE
+            cartBottom()
         })
 
         binding.accountContainer.setOnClickListener(View.OnClickListener {
-            if (viewmodel.currentTab == viewmodel.ACCOUNT) return@OnClickListener
-            viewmodel.currentTab = viewmodel.ACCOUNT
-            binding.pager.currentItem = 0
-
-            binding.accountContainerBG.setBackgroundResource(R.drawable.bottom_bar_item_bg)
-            binding.homeContainerBG.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.transparent))
-            binding.cartContainerBG.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.transparent))
-
-            binding.accountContainerBG.translationY = (binding.bottomContainer.height - binding.accountContainerBG.height).toFloat()
-            binding.accountContainerBG.animate().setDuration(200).translationY(0f).setListener(null)
-
-
-            params1.weight = 2f
-            binding.accountContainer.layoutParams = params1;
-
-            params2.weight = 3f
-            binding.cartContainer.layoutParams = params2;
-            binding.homeContainer.layoutParams = params2;
-
-            binding.homeIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_main_not_active))
-            binding.cartIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_cart_not_active))
-            binding.accountIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_account_active))
-
-            binding.homeIcontTv.visibility = View.GONE
-            binding.cartIconTv.visibility = View.GONE
-            binding.accountIconTv.visibility = View.VISIBLE
-
-
+            accountBottom()
         })
 
         binding.bottomHeaderView.setOnClickListener(View.OnClickListener {
@@ -175,19 +94,117 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    private fun accountBottom() {
+        val params1 : LinearLayout.LayoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
+        val params2 : LinearLayout.LayoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
+
+        if (viewmodel.currentTab == viewmodel.ACCOUNT) return
+        viewmodel.currentTab = viewmodel.ACCOUNT
+        binding.pager.currentItem = 0
+
+        binding.accountContainerBG.setBackgroundResource(R.drawable.bottom_bar_item_bg)
+        binding.homeContainerBG.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.transparent))
+        binding.cartContainerBG.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.transparent))
+
+        binding.accountContainerBG.translationY = (binding.bottomContainer.height - binding.accountContainerBG.height).toFloat()
+        binding.accountContainerBG.animate().setDuration(200).translationY(0f).setListener(null)
+
+
+        params1.weight = 2f
+        binding.accountContainer.layoutParams = params1;
+
+        params2.weight = 3f
+        binding.cartContainer.layoutParams = params2;
+        binding.homeContainer.layoutParams = params2;
+
+        binding.homeIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_main_not_active))
+        binding.cartIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_cart_not_active))
+        binding.accountIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_account_active))
+
+        binding.homeIcontTv.visibility = View.GONE
+        binding.cartIconTv.visibility = View.GONE
+        binding.accountIconTv.visibility = View.VISIBLE
+
+    }
+
+    private fun cartBottom() {
+        val params1 : LinearLayout.LayoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
+        val params2 : LinearLayout.LayoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
+
+        if (viewmodel.currentTab == viewmodel.CART) return
+        viewmodel.currentTab = viewmodel.CART
+        binding.pager.currentItem = 1
+
+        binding.cartContainerBG.setBackgroundResource(R.drawable.bottom_bar_item_bg)
+        binding.homeContainerBG.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
+        binding.accountContainerBG.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
+
+        binding.cartContainerBG.translationY = (binding.bottomContainer.height - binding.cartContainerBG.height).toFloat()
+        binding.cartContainerBG.animate().setDuration(200).translationY(0f).setListener(null)
+
+
+
+        params1.weight = 2f
+        binding.cartContainer.layoutParams = params1;
+
+        params2.weight = 3f
+        binding.homeContainer.layoutParams = params2;
+        binding.accountContainer.layoutParams = params2;
+
+        binding.homeIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_main_not_active))
+        binding.cartIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_cart_active))
+        binding.accountIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_account_not_active))
+
+        binding.homeIcontTv.visibility = View.GONE
+        binding.cartIconTv.visibility = View.VISIBLE
+        binding.accountIconTv.visibility = View.GONE
+    }
+
+    private fun homeBottom() {
+        val params1 : LinearLayout.LayoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
+        val params2 : LinearLayout.LayoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
+
+        if (viewmodel.currentTab == viewmodel.HOME) return
+        viewmodel.currentTab = viewmodel.HOME
+        binding.pager.currentItem = 2
+
+        binding.homeContainerBG.setBackgroundResource(R.drawable.bottom_bar_item_bg)
+        binding.cartContainerBG.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
+        binding.accountContainerBG.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
+
+        binding.homeContainerBG.translationY = (binding.bottomContainer.height- binding.homeContainerBG.height).toFloat()
+        binding.homeContainerBG.animate().setDuration(200).translationY(0f).setListener(null)
+
+
+        params1.weight = 2f
+        binding.homeContainer.layoutParams = params1;
+
+        params2.weight = 3f
+        binding.cartContainer.layoutParams = params2;
+        binding.accountContainer.layoutParams = params2;
+
+        binding.homeIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_main_active))
+        binding.cartIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_cart_not_active))
+        binding.accountIcon.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_account_not_active))
+
+        binding.homeIcontTv.visibility = View.VISIBLE
+        binding.cartIconTv.visibility = View.GONE
+        binding.accountIconTv.visibility = View.GONE
+    }
+
 
     private inner class HomePagerAdapter constructor(
             fm : FragmentManager,
-            behavior: Int,
+            lifecycle: Lifecycle,
             val homefragment : HomeFragment,
             val cartFragment: CartFragment,
             val accountFragment: AccountFragment
-    ) : FragmentStatePagerAdapter(fm, behavior){
+    ) : FragmentStateAdapter(fm,lifecycle){
 
 
-        override fun getCount(): Int = 3
+        override fun getItemCount(): Int = 3
 
-        override fun getItem(position: Int): Fragment {
+        override fun createFragment(position: Int): Fragment {
             if (position == 0) return accountFragment
             if (position == 1) return cartFragment
             if (position == 2) return homefragment
